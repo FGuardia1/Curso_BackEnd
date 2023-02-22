57ceaa2838efce184ac0f83925e51de218af20ca
Fernando Guardia <fguardia321@gmail.com>
2023-02-22 16:56:23 -0300

    desafio clase 42

--- clase42_desafio/tests/axios.test.js
+++ clase42_desafio/tests/axios.test.js
@@ -1,18 +1,25 @@
 import axios from "axios";
-const product = {
+const newProduct = {
   title: "prueba",
   price: 122,
   thumbnail: "imagen.jpg",
 };
 
 axios.defaults.baseURL = "http://localhost:8080";
-await axios.post("/product", product);
+//obtener productos
 let prods = await axios.get("/product");
 console.log("prods " + JSON.stringify(prods.data));
-//let resp = await axios.delete("/product/2");
-//console.log("prod eliminado" + JSON.stringify(resp.data));
-let resp = await axios.put("/product/4", {
+
+//crear producto
+await axios.post("/product", newProduct);
+
+//modificar producto
+resp = await axios.put("/product/4", {
   title: "modificado",
   price: 222,
   thumbnail: "imagen.jpg",
 });
+
+//eliminar producto
+let resp = await axios.delete("/product/4");
+console.log("prod eliminado" + JSON.stringify(resp.data));
